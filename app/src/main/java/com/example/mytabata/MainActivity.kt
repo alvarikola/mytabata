8fa985b59d224ff07520d5afb4367e83047c59f4
alvarikola <alvaroe484@gmail.com>
2024-10-15 12:53:47 +0100

    remember mutablestate

--- app/src/main/java/com/example/mytabata/MainActivity.kt
+++ app/src/main/java/com/example/mytabata/MainActivity.kt
@@ -11,6 +11,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mytabata.ui.theme.MytabataTheme
@@ -33,6 +37,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
+    var theCounter by remember { mutableStateOf("00") }
     Column {
         Text(
             text = "00",
